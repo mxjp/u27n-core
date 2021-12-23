e51bb30a55628b6dd4e341c01e1e305b0f06fcfd
Max J. Polster <max.polster@id-p.de>
2021-12-23 15:34:50 +0100

    Use line map package

--- src/source.ts
+++ src/source.ts
@@ -1,15 +1,14 @@
+import { LineMap } from "@mpt/line-map";
 import { isAbsolute, join, normalize, relative } from "path";
 
 import type { FragmentIdGenerator } from "./fragment-id-generator.js";
 import type { TranslationData } from "./translation-data.js";
-import { binarySearchIndex } from "./utility/binary-search.js";
 
 export class Source {
 	public readonly content: string;
-
 	public readonly fragmentIdGenerator?: FragmentIdGenerator;
 
-	private _lineMap: number[] | undefined = undefined;
+	private _lineMap: LineMap | undefined = undefined;
 	private _fragments: Source.Fragment[] | undefined = undefined;
 	private _fragmentMap: Map<string, Source.Fragment> | undefined = undefined;
 
@@ -28,6 +27,17 @@ export class Source {
 	 * Called to create an updated version of this source.
 	 */
 	public update?(context: Source.UpdateContext): Source.UpdateResult;
+
+	/**
+	 * A line map for this source that can be used
+	 * for converting between line/character positions and offsets.
+	 */
+	public get lineMap(): LineMap {
+		if (this._lineMap === undefined) {
+			this._lineMap = new LineMap(this.content);
+		}
+		return this._lineMap;
+	}
 
 	/**
 	 * An array of all fragments in this source.
@@ -57,59 +67,6 @@ export class Source {
 			this._fragmentMap = map;
 		}
 		return this._fragmentMap;
-	}
-
-	/**
-	 * An array of inclusive indices where each line starts.
-	 *
-	 * Supported line breaks are CRLF and LF.
-	 *
-	 * The value at each index represents the offset of the first character of that line. Line feeds are interpreted as part of the previous line.
-	 */
-	public get lineMap(): number[] {
-		if (this._lineMap === undefined) {
-			const map: number[] = [0];
-			let offset = -1;
-			while ((offset = this.content.indexOf("\n", offset += 1)) !== -1) {
-				map.push(offset + 1);
-			}
-			this._lineMap = map;
-		}
-		return this._lineMap;
-	}
-
-	/**
-	 * Convert a source location to an offset.
-	 *
-	 * @returns The offset of the character that the specified location points to or undefined if the location does not exist.
-	 */
-	public locationToOffset(location: Source.Location): number | undefined {
-		const { lineMap, content } = this;
-		if (location.line < 0 || location.column < 0 || location.line >= lineMap.length) {
-			return undefined;
-		}
-		const start = lineMap[location.line];
-		const length = ((location.line + 1 < lineMap.length) ? lineMap[location.line + 1] : content.length) - start;
-		if (location.column >= length) {
-			return undefined;
-		}
-		return start + location.column;
-	}
-
-	/**
-	 * Convert a source offset to a location.
-	 *
-	 * @returns The location of the character that is specified by the offset or undefined if the offset does not exist.
-	 */
-	public offsetToLocation(offset: number): Source.Location | undefined {
-		const { lineMap, content } = this;
-		const line = binarySearchIndex(lineMap, (start, line) => {
-			const end = line === lineMap.length - 1 ? content.length : lineMap[line + 1];
-			return offset < start ? -1 : (offset >= end ? 1 : 0);
-		});
-		return line === undefined
-			? undefined
-			: { line, column: offset - lineMap[line] };
 	}
 
 	/**
@@ -145,13 +102,6 @@ export declare namespace Source {
 		start: number;
 		/** The exclusive end offset */
 		end: number;
-	}
-
-	export interface Location {
-		/** The line index */
-		line: number;
-		/** The column index */
-		column: number;
 	}
 
 	export interface UpdateContext {
--- test/source.ts
+++ test/source.ts
@@ -1,57 +1,14 @@
+import { LineMap } from "@mpt/line-map";
 import test from "ava";
 import { join } from "path";
 
 import { Source } from "../src/source.js";
-import { unindent } from "./_utility/unindent.js";
 
 test("lineMap", t => {
-	([
-		["", [0]],
-		["\n", [0, 1]],
-		[`foo\nbar`, [0, 4]],
-		[`
-			Hello World!
-			foo
-			bar
-		`, [0, 1, 14, 18, 22]],
-	] as [string, number[]][]).forEach(([content, lineMap]) => {
-		const source = new Source(unindent(content));
-		t.deepEqual(source.lineMap, lineMap);
-	});
-});
-
-test("locationToOffset / offsetToLocation", t => {
-	const source = new Source(`foo\nbar\nbaz`);
-	([
-		[-1, undefined],
-		[undefined, { line: -1, column: 0 }],
-		[undefined, { line: 0, column: -1 }],
-		[0, { line: 0, column: 0 }],
-		[1, { line: 0, column: 1 }],
-		[2, { line: 0, column: 2 }],
-		[3, { line: 0, column: 3 }],
-		[undefined, { line: 0, column: 4 }],
-		[undefined, { line: 1, column: -1 }],
-		[4, { line: 1, column: 0 }],
-		[5, { line: 1, column: 1 }],
-		[6, { line: 1, column: 2 }],
-		[7, { line: 1, column: 3 }],
-		[undefined, { line: 1, column: 4 }],
-		[undefined, { line: 2, column: -1 }],
-		[8, { line: 2, column: 0 }],
-		[9, { line: 2, column: 1 }],
-		[10, { line: 2, column: 2 }],
-		[undefined, { line: 2, column: 3 }],
-		[11, undefined],
-	] as [number | undefined, Source.Location | undefined][]).forEach(([offset, location]) => {
-		if (location !== undefined) {
-			t.is(source.locationToOffset(location), offset);
-		}
-		if (offset !== undefined) {
-			t.deepEqual(source.offsetToLocation(offset), location);
-		}
-	});
-	t.pass();
+	const source = new Source("foo\nbar");
+	t.true(source.lineMap instanceof LineMap);
+	t.is(source.lineMap.text, source.content);
+	t.deepEqual(source.lineMap.offsets, [0, 4]);
 });
 
 test("fragments / fragmentMap", t => {
